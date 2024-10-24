5ac281a50e9725cd6f53ea33c76871609d48a546
noa0610 <human.kagoshima.yamanaka@gmail.com>
2024-10-24 15:57:28 +0900

    [add] 敵ウェーブ生成スクリプト(仮)追加
    ・プレハブ化された敵を生成するためのEnemyGeneratorスクリプト追加
    ・テストプレイのために、インスペクター上で出現させる情報を設定できる仕組み
    ・各ウェーブごとに、どの敵を、何体、何秒後に、どの方向から、何秒間隔で出現させるか選べる。

--- GFF_AWARD_2024 Unity/Assets/2,Scripts/GameManager/TestScoreText.cs
+++ GFF_AWARD_2024 Unity/Assets/2,Scripts/GameManager/TestScoreText.cs
@@ -5,23 +5,23 @@ using TMPro;
 
 public class TestScoreText : MonoBehaviour
 {
-    public TextMeshProUGUI tmpro;
-    public int score = 0;
+    public TextMeshProUGUI tmpro; // 変更するテキスト
+    public int score = 0; // テキストのスコア
     void Start()
     {
         if(tmpro == null)
         {
             tmpro = this.GetComponent<TextMeshProUGUI>();
         }
-        tmpro.text = $"{score}";
+        tmpro.text = $"{score}"; // テキストにスコアを反映
     }
 
     void Update()
     {
-        if(score != ScoreManager.instance.GetScore())
+        if(score != ScoreManager.instance.GetScore()) // テキストのスコアとScoreManagerのスコアに違いがあれば
         {
-            score = ScoreManager.instance.GetScore();
-            tmpro.text = $"{score}";
+            score = ScoreManager.instance.GetScore(); // ScoreManagerのスコアを反映
+            tmpro.text = $"{score}"; // テキストのスコアを変更
         }
     }
 }
